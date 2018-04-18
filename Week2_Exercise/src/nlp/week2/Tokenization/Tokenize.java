a3c2ed8ee078a1f0ca4ab09d9e5cb1fb9e41cde7
DKNeko <38471859+DKNeko@users.noreply.github.com>
2018-04-19 01:08:06 +0700

    Update token

--- Week2_Exercise/src/nlp/week2/Tokenization/Tokenize.java
+++ Week2_Exercise/src/nlp/week2/Tokenization/Tokenize.java
@@ -1,6 +1,30 @@
 package nlp.week2.Tokenization;
 
+import java.io.*;
+import java.util.*;
+
+import edu.stanford.nlp.ling.*;
+import edu.stanford.nlp.parser.lexparser.*;
+import edu.stanford.nlp.process.*;
+import edu.stanford.nlp.trees.*;
+
+
 public class Tokenize {
+LexicalizedParser lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
+TokenizerFactory<CoreLabel> tokenizerFactory = PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
+Tokenizer<CoreLabel> tok = tokenizerFactory.getTokenizer(new StringReader("This is an example text"));
+java.util.List<CoreLabel> rawWords = tok.tokenize();
+Tree parse = lp.apply(rawWords);
+TreebankLanguagePack tlp = lp.treebankLanguagePack();
+GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
+for (TypedDependency tdl_token : tdl) {
+
+String w = tdl_token.dep().word();
+System.out.println(w);
+
+}
 }
 
 
